d49b0cc96cfb782bb721e8783ec4072a334c8d61
Yezid <ygm.mecatronico@gmail.com>
2025-09-06 20:10:54 -0500

    solucion bug 6° , datanotations para valores requeridos y longuitud de texto en datos del formulario

--- JobBoard.Web/Models/ViewModels/Recruiter/JobOfferForm.cs
+++ JobBoard.Web/Models/ViewModels/Recruiter/JobOfferForm.cs
@@ -1,15 +1,26 @@
 ﻿using JobBoard.Common.Enums;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
 
 namespace JobBoard.Web.Models.ViewModels.Recruiter
 {
     public class JobOfferForm
     {
+        [Required, StringLength(120)]
         public string Title { get; set; }
+
+        [Required, StringLength(4000)]
         public string Description { get; set; }
+
+        [Required, StringLength(120)]
         public string Location { get; set; }
+
+        [Required]
+        [Range(0, 100000000)]
         public decimal Salary { get; set; }
+
+        [Required]
         public ContractType ContractType { get; set; }
         public bool IsActive { get; set; } = true;
 
